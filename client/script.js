a1e885b593c610be0605a8e7668c541682bc9bb3
muhammedgundogdu <muhammedgundogdu45@gmail.com>
2022-12-27 19:32:42 +0300

    update the server url

--- client/script.js
+++ client/script.js
@@ -77,7 +77,7 @@ const handleSubmit = async (e) => {
 
   //fetch data from server -> bot's response
 
-  const response = await fetch("https://codex-f6ja.onrender.com", {
+  const response = await fetch("https://codex-f6ja.onrender.com/", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
